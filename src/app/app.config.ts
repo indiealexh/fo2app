086f32ce5d27eb1907c38a26033bb9952c62d57a
Alexander Haslam <aihaslam@ucdavis.edu>
2025-02-14 21:16:28 -0800

    Enable Progressive Web App (PWA) support
    Added PWA features including a web manifest, service worker configuration, and app icons for various resolutions. Configured Angular to use the service worker and updated necessary files to support offline capabilities and enhanced user experience.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,13 @@
-import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, isDevMode, provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideExperimentalZonelessChangeDetection(), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [provideExperimentalZonelessChangeDetection(), provideRouter(routes), provideClientHydration(withEventReplay()), provideServiceWorker('ngsw-worker.js', {
+    enabled: !isDevMode(),
+    registrationStrategy: 'registerWhenStable:30000'
+  }),]
 };
