d0ed93577f7ae9ace4a9d54afdf571b9afcd8109
Alexander Haslam <aihaslam@ucdavis.edu>
2025-02-19 18:59:29 -0800

    Refine level ranges and zero difference values in xp.ts
    Revised the logic for zero difference calculations by breaking down player level ranges more precisely and adjusting their corresponding values. These changes improve granularity and ensure better alignment with level progression.

--- src/app/xp-functions/xp.ts
+++ src/app/xp-functions/xp.ts
@@ -1,13 +1,24 @@
 export function getZeroDifference(playerLevel: number) {
   let zd = 0;
-  if (playerLevel >= 1 && playerLevel <= 9) zd = 1;
-  if (playerLevel >= 10 && playerLevel <= 15) zd = 8;
-  if (playerLevel >= 20 && playerLevel <= 29) zd = 10;
+  if (playerLevel >= 1 && playerLevel <= 7) zd = 5;
+  if (playerLevel >= 8 && playerLevel <= 9) zd = 6;
+  if (playerLevel >= 10 && playerLevel <= 11) zd = 7;
+  // 15 = 8
+  if (playerLevel >= 12 && playerLevel <= 15) zd = 8;
+  // 16 - 19 = 9
+  if (playerLevel >= 16 && playerLevel <= 19) zd = 9;
+  // 20 - 28 = 11
+  if (playerLevel >= 20 && playerLevel <= 29) zd = 11;
   if (playerLevel >= 30 && playerLevel <= 39) zd = 12;
-  if (playerLevel >= 40 && playerLevel <= 49) zd = 14;
-  if (playerLevel >= 50 && playerLevel <= 59) zd = 16;
-  if (playerLevel >= 60 && playerLevel <= 69) zd = 18;
+  // 40 = 14
+  if (playerLevel >= 40 && playerLevel <= 44) zd = 14;
+  if (playerLevel >= 45 && playerLevel <= 49) zd = 14;
+  if (playerLevel >= 50 && playerLevel <= 54) zd = 15;
+  if (playerLevel >= 55 && playerLevel <= 59) zd = 16;
+  if (playerLevel >= 60 && playerLevel <= 64) zd = 17;
+  // 65 = 18
+  if (playerLevel >= 65 && playerLevel <= 69) zd = 18;
   if (playerLevel >= 70 && playerLevel <= 79) zd = 20;
-  console.log("ZD:",zd);
+  console.log("ZD:", zd);
   return zd;
 }
