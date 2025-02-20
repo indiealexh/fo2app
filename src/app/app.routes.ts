a4a70a47e4842b587f65faa6cc7c12d92c4f7ae3
Alexander Haslam <aihaslam@ucdavis.edu>
2025-02-20 15:00:26 -0800

    Switch to lazy loading for route components
    Replaced direct component imports with lazy loading for 'mobs/value' and 'xp' routes. This improves initial load time and optimizes performance by loading components only when needed.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
-import { PageMobValueComponent } from './pages/page-mob-value/page-mob-value.component';
-import { PageXpGraphComponent } from './pages/page-xp-graph/page-xp-graph.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: '/mobs/value', pathMatch: 'full'},
-  {path: 'mobs/value', pathMatch: 'full', component: PageMobValueComponent},
-  {path: 'xp', pathMatch: 'full', component: PageXpGraphComponent},
+  {
+    path: 'mobs/value',
+    pathMatch: 'full',
+    loadComponent: () => import('./pages/page-mob-value/page-mob-value.component').then(m => m.PageMobValueComponent)
+  },
+  {
+    path: 'xp',
+    pathMatch: 'full',
+    loadComponent: () => import('./pages/page-xp-graph/page-xp-graph.component').then(m => m.PageXpGraphComponent)
+  },
   {path: '**', redirectTo: '/mobs/value'}
 ];
