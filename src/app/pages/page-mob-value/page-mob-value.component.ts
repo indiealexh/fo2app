27fcdd1048aac92928bb5b98cf81b3fac2439b7b
Alexander Haslam <aihaslam@ucdavis.edu>
2025-02-22 17:13:52 -0800

    Fix conditional handling of attack speed in mob value calc
    Previously, calculations did not account for scenarios where `atkSpeed` is zero, potentially leading to incorrect kill rates. This update ensures proper handling by introducing a condition to adjust `killsPerSecond` calculation when `atkSpeed` equals zero.

--- src/app/pages/page-mob-value/page-mob-value.component.ts
+++ src/app/pages/page-mob-value/page-mob-value.component.ts
@@ -159,9 +159,9 @@ export class PageMobValueComponent {
           const baseXP = m.level ? ((5 * (m.level - 1)) + 50) : 0;
           const estXP = calcXPDrop(m.level ?? 0, this.lvl());
           const xpPerHp = estXP / (m.health ?? 0);
-          const killsPerSecond = this.dps() / (m.health ?? 1);
+          let killsPerSecond = this.dps() / (m.health ?? 1);
           if (m.atkSpeed == 0) {
-            const killsPerSecond = 1 / (m.health ?? 1);
+            killsPerSecond = 1 / (m.health ?? 1);
           }
           const killsPerMinute = killsPerSecond * 60;
 
